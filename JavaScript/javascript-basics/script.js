321091f40b6be1485e7620d10ce422d5210873f4
Mohammed qubaisuddin <hiq4dsa@gmail.com>
2021-08-05 20:39:30 +0530

    full stack development

---
+++ JavaScript/javascript-basics/script.js
@@ -0,0 +1,6 @@
+console.log("hello world! mohammed qubaisuddin is my name printed second")
+window.alert("again dont browse ")
+
+
+//we can actually write window.alert()
+//and we can also simply write alert()

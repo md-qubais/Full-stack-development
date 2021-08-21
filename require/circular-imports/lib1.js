9c476458593761e887d24fa698b82faa4ce135ec
Mohammed qubaisuddin <hiq4dsa@gmail.com>
2021-08-21 18:18:31 +0530

    updated

--- require/circular-imports/lib1.js
+++ require/circular-imports/lib1.js
@@ -2,4 +2,6 @@ const lib2=require("./lib2")
 module.exports={
     a:10,lib2
 }
-console.log("here executing ")
+//so here the lib1 and lib2 are in the main stack for resolving
+//the require if the require didnt resolved and agina we are calling them to require
+//than it will return the empty object
--- require/circular-imports/main.js
+++ require/circular-imports/main.js
@@ -4,3 +4,10 @@ const lib2=require("./lib2")
 
 console.log(lib1)
 console.log(lib2)
+
+console.log(lib1.lib2)
+console.log(lib2.lib1)
+
+//so here the lib1 and lib2 are in the main stack for resolving
+//the require if the require didnt resolved and agina we are calling them to require
+//than it will return the empty object

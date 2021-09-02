bebcab825319b0b28c88abf220a4e4f50a436388
Mohammed qubaisuddin <hiq4dsa@gmail.com>
2021-09-02 23:54:18 +0530

    React todo

---
+++ Reactjs/React_TODO/todo/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+  
+    <Todo></Todo>
+  );
+}
+
+export default App;
---
+++ Reactjs/React_TODO/todo/src/components/Todo.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    state={
+        tasklist:[],
+        curr_task:"",
+    }
+    deleteTask =(curr_task)=>{
+        let filtered_array=this.state.tasklist.filter((task)=>{
+            return task.id!==curr_task.id;
+        })
+        // when then setState encounters or
+        //any state goes to any change then the react
+        //will rendered it on the browser
+        this.setState({
+            tasklist : filtered_array
+        })
+    }
+    handleCurrtask=(e)=>{
+        let currValue=e.target.value;
+        this.setState({
+            curr_task:currValue,       
+        })
+    }
+    addTask=()=>{
+        let array=this.state.tasklist;
+        array.push({id:this.state.tasklist.length ,task: this.state.curr_task});
+        this.setState({
+            curr_task:"",
+            tasklist:array
+        })
+    }
+    render() {
+        return (
+
+            <div>
+                <div className="input-container">
+                    <input type="text" value={this.state.curr_task} onChange={(e)=>{this.handleCurrtask(e)}} />
+                    <button onClick={()=>{this.addTask()}}>Submit</button>
+                </div>
+               {/* here we have to write {} for evaluation for jsx */}
+               <ul>
+               {
+                   this.state.tasklist.map((taskobj)=>{
+                       return (
+                           <li className="classList" key={taskobj.id}>
+                               <p>{taskobj.task}</p>
+                               <button onClick={()=>{this.deleteTask(taskobj.task)}}>Delete</button>
+                           </li>
+                       )                       
+                   })
+               }
+               </ul>
+            </div>
+        )
+    }
+}
